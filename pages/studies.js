15855a4b0532a39eab8e579f3d5d3f225f4a57be
Joshua Petitma <joshua@petit.dev>
2022-06-26 17:01:32 -0700

    Update studies page to not have default templates

--- pages/studies.js
+++ pages/studies.js
@@ -13,7 +13,7 @@ export default function Projects() {
             Bible Studies
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            Bible studies that I believe will help you to grow your relationship with Christ
           </p>
         </div>
         <div className="container py-12">
