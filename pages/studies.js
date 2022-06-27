f1d6e2b78214c8d29002959f1f383f93d39a5989
Joshua Petitma <joshua@petit.dev>
2022-06-27 10:48:35 -0700

    Update bible studies tab title

--- pages/studies.js
+++ pages/studies.js
@@ -6,14 +6,17 @@ import { PageSEO } from '@/components/SEO'
 export default function Projects() {
   return (
     <>
-      <PageSEO title={`Projects - ${siteMetadata.author}`} description={siteMetadata.description} />
+      <PageSEO
+        title={`Bible Studies - ${siteMetadata.author}`}
+        description={siteMetadata.description}
+      />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Bible Studies
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Bible studies that I believe will help you to grow your relationship with Christ
+            Bible studies that I believe will help you grow your relationship with Christ
           </p>
         </div>
         <div className="container py-12">
