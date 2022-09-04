cb67fd4758689848487b32e6e88636a6c6c8cacb
Joshua Petitma <joshua@petit.dev>
2022-09-04 09:06:57 -0400

    Update :)

--- pages/api/emailoctopus.js
+++ pages/api/emailoctopus.js
@@ -1,7 +1,6 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req, res) => {
   const { email } = req.body
-  console.log(process.env)
   if (!email) {
     return res.status(400).json({ error: 'Email is required' })
   }
